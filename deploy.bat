d30715f71f03195978008f4e12f57054f3c90318
Joshua <mama27j@gmail.com>
2019-09-15 20:29:13 +0300

    update proxy

--- deploy.bat
+++ deploy.bat
@@ -3,5 +3,5 @@
 set name= bfastapp
 docker swarm init || echo 'pass initialize swarm mode'
 docker network create -d overlay bfastweb || echo 'pass network creation...'
-docker-compose -p %name% build
+REM docker-compose -p %name% build
 docker stack deploy -c ./docker-compose.yml %name% 
