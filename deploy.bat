25ad753bd4efd0efcadca0408b64179d2cdb1979
Joshua <mama27j@gmail.com>
2019-09-15 14:23:36 +0300

    refresh

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,4 @@ set name= bfastapp
 docker swarm init || echo 'pass initialize swarm mode'
 docker network create -d overlay bfastweb || echo 'pass network creation...'
 docker-compose -p %name% build
-docker stack deploy %name% 
+docker stack deploy -c ./docker-compose.yml %name% 
