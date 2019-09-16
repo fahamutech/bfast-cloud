77b27b5a11e916eaad97ff8d27e9a15ff88e246e
Joshua <mama27j@gmail.com>
2019-09-16 10:03:33 +0300

    update

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,107 +1,45 @@
-const pStore = new Vuex.Store({
+const appStore = new Vuex.Store({
     state: {
-        projects: [],
     },
     mutations: {
-        projects(state, data){
-            state.projects = data;
-        }
+       
     },
     actions: {
-        projects(context){
-            axios.post('/project/all',{
-                uid: 'joshua'
-            }).then(value=>{
-                // console.log(value);
-                context.commit('projects', value.data);
-            }).catch(reason=>{
-                context.commit('projects', []);
-                console.log(reason.response); 
-            });
-        },
+       
     }
 });
 
+const isLogin = false;
 
-const project = new Vue({
-    el: '#projects',
-    store: pStore,
+const appAuth = new Vue({
+    el: '#appAuth',
+    store: appStore,
     data: {
-        
+        loginProgress: false,
+        isLogin: isLogin,
+        user: {
+            username: '',
+            password: ''
+        }
     },
     created: function(){
-        this.$store.dispatch('projects');
+       
     },
     methods: {
-
+        login(){
+            console.log('implement login');
+        },
+        register(){
+            console.log('implement register');
+        },
+        logout(){
+            console.log('implement logout');
+        },
+        
     },
     computed: {
-        projects: function(){
-            // console.log(this.$store.state.projects);
-            return this.$store.state.projects;
-        },
-        showProjcts: function(){
-            
-        }
-    }
-});
-
-const project_new = new Vue({
-    el: '#createProject',
-    store: pStore,
-    created(){
-        console.log('new project form created');
-    },
-    data: {
-        createProjectMessage: '',
-        createProjectError: false,
-        createProjectDone: false,
-        createProgressFlag: false,
-        project: {
-            name: '',
-            projectId: '',
-            description: ''
-        }
-    },
-    methods: {
-        createProject: function(){
-            this.createProjectDone = false;
-            this.createProjectError = false;
-            const form = this.$refs.createProjectForm;
-            const createB = this.$refs.createButton;
-            const closeB = this.$refs.closeButton;
-            const valid = form.checkValidity();
-            form.classList.add('was-validated');
-            console.log(this.project);
-            if(valid){
-                this.createProgressFlag = true;
-                createB.classList.add('disabled');
-                closeB.classList.add('disabled');
-                const project = this.project;
-                project.user = {
-                    uid: 'joshua'
-                };
-                axios.post('/project', project).then(value=>{
-                    this.project.name ='';
-                    this.project.projectId = '';
-                    this.project.description = '';
-                    this.createProjectDone = true;
-                    this.$store.dispatch('projects');
-                }).catch(error=>{
-                    this.createProjectError = true;
-                    this.createProjectMessage = error.response.data.message;
-                    // console.log(error.response);
-                }).finally(_=>{
-                    createB.classList.remove('disabled');
-                    closeB.classList.remove('disabled');
-                    this.createProgressFlag = false;
-                });
-            }
-        },
-    },
-    computed: {
-        showProgress: function(){
-            return this.createProgressFlag;
+        getIsLogin(){
+            return this.isLogin;
         }
     }
 });
