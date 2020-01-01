f0139799f6e098860d95fb37cae1a420899b1e7f
joshua <mama27j@gmail.com>
2020-01-01 14:21:42 +0300

    update compose files

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ name=bfast
 docker swarm init || echo 'pass initialize swarm mode'
 docker network create -d overlay --attachable bfastnet || echo 'pass network creation...'
 # docker-compose -p ${name} build
-docker stack deploy -c ./docker-compose.yml --with-registry-auth  ${name}
+docker stack deploy -c ./docker-compose.yml --with-registry-auth ${name}
