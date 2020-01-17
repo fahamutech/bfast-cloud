63e1d56563690df39495aaccdf85b57de807a83f
joshua <mama27j@gmail.com>
2020-01-17 09:49:05 +0300

    update deploy.sh deploy.bat to bfastweb network

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-name=bfast
+name=bfastapp
 docker swarm init || echo 'pass initialize swarm mode'
-docker network create -d overlay --attachable bfastnet || echo 'pass network creation...'
+docker network create -d overlay --attachable bfastweb || echo 'pass network creation...'
 # docker-compose -p ${name} build
 docker stack deploy -c ./docker-compose.yml --with-registry-auth ${name}
