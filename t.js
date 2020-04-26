83d51888c310189720e760995a6e352efabf12e2
joshua <mama27j@gmail.com>
2020-04-26 19:20:00 +0300

    add environment to bfast project faas component

---
+++ t.js
@@ -0,0 +1,13 @@
+console.log(JSON.stringify({
+        module: 'parse-server-s3like-adapter',
+        options: {
+            accessKey: 'F1NJCOXZH9TODM7IR0TZ',
+            bucket: 'test',
+            direct: true,
+            endPoint: 'https://eu-central-1.linodeobjects.com',
+            secretKey: 'XFUMm6K8p9xjMXTNMWFNbphKpdtKDfk7X10sogjh'
+        }
+    }
+));
+
+
