e837c9b824a0cca461c00520ec92327aa482c329
joshua <mama27j@gmail.com>
2020-04-27 23:30:41 +0300

    add faas and ssm migration

--- daas-migration.sh
+++ daas-migration.sh
@@ -3,4 +3,4 @@ docker service update --force --env-add 'PARSE_SERVER_LIVE_QUERY={"classNames":[
 --env-add 'S3_ACCESS_KEY=5IGXSX5CU52C2RFZFALG' \
 --env-add 'S3_SECRET_KEY=2q2vteO9lQp6LaxT3lGMLdkUF5THdxZWmyWmb1y9' \
 --env-add 'S3_ENDPOINT=https://eu-central-1.linodeobjects.com/' \
---image joshuamshana/bfast-ce-daas:v0.1.1 $2
+--image joshuamshana/bfast-ce-daas:v0.1.1 $1_daas
