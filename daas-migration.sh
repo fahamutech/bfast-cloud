bfc9289941dd33cdc30237a1c435eff2788365f1
joshua <mama27j@gmail.com>
2020-04-27 21:57:12 +0300

    add migrate for daas

--- daas-migration.sh
+++ daas-migration.sh
@@ -1,6 +1,6 @@
-docker service update --force --label-add 'PARSE_SERVER_LIVE_QUERY={"classNames":[],redisURL:"redis://rdb:6379"}' \
---label-add 'S3_BUCKET=bfast-${projectId}' \
---label-add 'S3_ACCESS_KEY=5IGXSX5CU52C2RFZFALG' \
---label-add 'S3_SECRET_KEY=2q2vteO9lQp6LaxT3lGMLdkUF5THdxZWmyWmb1y9' \
---label-add 'S3_ENDPOINT=https://eu-central-1.linodeobjects.com/' \
+docker service update --force --env-add 'PARSE_SERVER_LIVE_QUERY={"classNames":[],redisURL:"redis://rdb:6379"}' \
+--env-add 'S3_BUCKET=bfast-${projectId}' \
+--env-add 'S3_ACCESS_KEY=5IGXSX5CU52C2RFZFALG' \
+--env-add 'S3_SECRET_KEY=2q2vteO9lQp6LaxT3lGMLdkUF5THdxZWmyWmb1y9' \
+--env-add 'S3_ENDPOINT=https://eu-central-1.linodeobjects.com/' \
 --image joshuamshana/bfast-ce-daas:v0.1.0 $1_daas
