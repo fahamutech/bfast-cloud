f2553813ca02d7f307a5f412c7e4b2efb5ddf4c6
joshua <mama27j@gmail.com>
2020-04-27 23:29:09 +0300

    add faas and ssm migration

---
+++ faas-migration.sh
@@ -0,0 +1,1 @@
+docker service update --force --image joshuamshana/bfastfaas:v1.9.0 $1
---
+++ ssm-migration.sh
@@ -0,0 +1,6 @@
+docker service update --force --env-add 'PARSE_SERVER_LIVE_QUERY={"classNames":["stocks","sales","purchases","units","categories","accounts","suppliers","orders"],redisURL:"redis://rdb:6379"}' \
+--env-add  S3_BUCKET=ssm-$1 \
+--env-add 'S3_ACCESS_KEY=5IGXSX5CU52C2RFZFALG' \
+--env-add 'S3_SECRET_KEY=2q2vteO9lQp6LaxT3lGMLdkUF5THdxZWmyWmb1y9' \
+--env-add 'S3_ENDPOINT=https://eu-central-1.linodeobjects.com/' \
+--image joshuamshana/bfast-ce-daas:v0.1.1 $1_ssm
