214e7fb77c72a8f93238fc8a798a4dcf58d2ac13
joshua <mama27j@gmail.com>
2020-05-04 22:44:47 +0300

    remove s3 volume plugin creation

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ if [ -e "$1" ]; then
   source "${configPath}"
   docker swarm init || echo 'pass initialize swarm mode'
   docker network create -d overlay --attachable bfastweb || echo 'pass network creation...'
-  docker network create -d overlay --attachable mongo-net || echo 'pass mongo network creation...'
+  # docker network create -d overlay --attachable mongo-net || echo 'pass mongo network creation...'
   # shellcheck disable=SC2154
-  docker plugin install --grant-all-permissions rexray/s3fs "${key}" "${secret}" "${endpoint}" "${s3Options}"
+  # docker plugin install --grant-all-permissions rexray/s3fs "${key}" "${secret}" "${endpoint}" "${s3Options}"
   if [ -e "$2" ]; then
     echo "Start deploying mongo cluster"
     bash "$2"
