a57be1e56a2cf4524f3bee53598920956939acf9
joshua <mama27j@gmail.com>
2020-05-04 22:32:00 +0300

    update mongodb ur

--- src/factory/ProjectStoreFactory.ts
+++ src/factory/ProjectStoreFactory.ts
@@ -40,7 +40,7 @@ export class ProjectStoreFactory implements ProjectStoreAdapter {
                     await projectColl.createIndex({projectId: 1}, {unique: true});
                 }
                 const adminColl = await _database.getDatabase('admin');
-                // testing
+                // add user for auth
                 try {
                     await adminColl.addUser(project.parse.appId, project.parse.masterKey, {
                         roles: [
--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import {BfastCloud} from "./bfast-cloud";
 new BfastCloud({
     devMode: process.env.DEBUG === 'true',
     port: process.env.PORT ? process.env.PORT : '3000',
-    masterKey: process.env.MASTER_KEY ? process.env.MASTER_KEY : 'qwertyuyttrwet7dfkgfger8966553333wt68746egckjegytgw79et9c7be',
+    masterKey: process.env.MASTER_KEY ? process.env.MASTER_KEY : 'qwertyuyttrwet7dfkgfger8966553333wt68746',
     redisHOST: process.env.REDIS_HOST ? process.env.REDIS_HOST : 'rdb',
-    mongoURL: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://mdb:27017,mdbrs1:27017,mdbrs2:27017/_BFAST_ADMIN?replicaSet=bfastRS',
-    mongoMasterURL: process.env.MONGO_MASTER_URL ? process.env.MONGO_MASTER_URL : 'mongodb://mdb:27017/_BFAST_ADMIN',
+    mongoURL: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/_BFAST_ADMIN',
+    mongoMasterURL: process.env.MONGO_MASTER_URL ? process.env.MONGO_MASTER_URL : 'mongodb://localhost:27017/_BFAST_ADMIN',
     dockerSocket: process.env.DOCKER_SOCKET ? process.env.DOCKER_SOCKET : '/usr/local/bin/docker'
 });
