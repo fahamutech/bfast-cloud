9a25f9f55eaf5068a39f6473daf061d20bdc3830
joshua <mama27j@gmail.com>
2020-09-13 10:04:33 +0300

    update deploy bash

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ if [ -e "$1" ]; then
   # shellcheck disable=SC1090
   source "${configPath}"
   docker swarm init || echo 'pass initialize swarm mode'
+  docker secret rm s3secretKey || echo 's3secretKey removed'
+  docker secret rm s3accessKey || echo 's3accessKey removed'
+  docker secret rm s3endpoint || echo 's3endpoint removed'
   docker secret create s3endpoint ./secrets/s3endpoint.txt
   docker secret create s3accessKey ./secrets/s3accessKey.txt
   docker secret create s3secretKey ./secrets/s3secretKey.txt
