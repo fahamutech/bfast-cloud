0cf98cb5ea51fb51a2baa4bf93a95ced07d98c23
joshua <mama27j@gmail.com>
2020-10-01 14:47:48 +0300

    update startup configurations

--- deploy.sh
+++ deploy.sh
@@ -8,19 +8,21 @@ if [ -e "$1" ]; then
   docker secret rm s3accessKey
   docker secret rm s3endpoint
   docker secret rm s3endpointUsEast1
-  docker secret create s3endpoint ./secrets/s3endpoint.txt
-  docker secret create s3endpointUsEast1 ./secrets/s3endpointUsEast1.txt
-  docker secret create s3accessKey ./secrets/s3accessKey.txt
-  docker secret create s3secretKey ./secrets/s3secretKey.txt
+  docker secret rm privkey
+  docker secret rm cert
+  docker secret create privkey "${privkey}"
+  docker secret create cert "${cert}"
+  docker secret create s3endpoint "${s3endpoint}"
+  docker secret create s3endpointUsEast1 "${s3endpointUsEast1}"
+  docker secret create "${s3accessKey}"
+  docker secret create s3secretKey "${s3secretKey}"
   docker network create -d overlay --attachable bfastweb || echo 'pass network creation...'
-  # shellcheck disable=SC2154
   if [ -e "$2" ]; then
     echo "Start deploying mongo cluster"
     bash "$2"
   else
     echo "mongo cluster not specified"
   fi
-  # shellcheck disable=SC2154
   docker stack deploy -c ./docker-compose.yml --with-registry-auth "${name}"
 else
   echo "please specify configPath as first argument"
