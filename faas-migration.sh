2ce645326f928c9f1f7bfabd2d56dc67d4b7fbce
joshua mshana <mama27j@gmail.com>
2020-12-04 10:30:48 +0300

    remove deployment info to specific repo

--- faas-migration.sh
+++
@@ -1,6 +0,0 @@
-docker service update --force --image joshuamshana/bfastfaas:latest "$1"_faas
-
-#--env-add  S3_BUCKET=bfast-$1 \
-#--env-add 'S3_ACCESS_KEY=5IGXSX5CU52C2RFZFALG' \
-#--env-add 'S3_SECRET_KEY=2q2vteO9lQp6LaxT3lGMLdkUF5THdxZWmyWmb1y9' \
-#--env-add 'S3_ENDPOINT=https://eu-central-1.linodeobjects.com/' \
