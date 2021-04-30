9275b3a61874d807b5264d4500d1826ade9af37d
joshua mshana <mama27j@gmail.com>
2021-04-30 16:04:02 +0300

    update configs for rsa

--- functions/options.mjs
+++ functions/options.mjs
@@ -65,7 +65,9 @@ export function getBFastDatabaseConfigs() {
         masterKey: options.masterKey,
         logs: envUtil.getEnv(process.env.LOGS) === '1',
         port: port ? port : '3000',
-        //   taarifaToken: envUtil.getEnv(process.env.TAARIFA_TOKEN),
+        taarifaToken: envUtil.getEnv(process.env.TAARIFA_TOKEN),
+        rsaKeyPairInJson: envUtil.getEnv(process.env.RSA_PRIV),
+        rsaPublicKeyInJson: envUtil.getEnv(process.env.RSA_PUB),
         mongoDbUri: options.mongoURL,
         adapters: {
             s3Storage: isS3Configured ? {
