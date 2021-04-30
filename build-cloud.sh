746df0e2cc1a70673fa01cc2e05294218eae8cc6
joshua mshana <mama27j@gmail.com>
2021-04-30 15:54:00 +0300

    install only dependencies

--- build-cloud.sh
+++ build-cloud.sh
@@ -1,4 +1,4 @@
 docker build --tag build-cloud .
-docker run --rm -v "${PWD}":/bfast build-cloud npm install
+docker run --rm -v "${PWD}":/bfast build-cloud npm install --only=production
 npm config set //registry.npmjs.org/:_authToken="${NPM_TOKEN}"
 npm publish
