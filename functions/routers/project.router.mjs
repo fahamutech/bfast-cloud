9e4c59b6521f4ae3617f01a9b75a543376c126f5
joshua mshana <mama27j@gmail.com>
2021-06-08 01:41:54 +0300

    update rsa keypair generation

--- functions/routers/project.router.mjs
+++ functions/routers/project.router.mjs
@@ -162,10 +162,10 @@ export const createNewProject = bfast.functions().onPostHttpRequest(`${prefix}/:
                     const envs = request.query.envs && request.query.envs.toString().startsWith('[') ? JSON.parse(request.query.envs) : [];
                     // console.log(envs);
                     // if(!envs.includes('RSA_PUBLIC_KEY')){
-                        envs.push(`"RSA_PUBLIC_KEY=${JSON.stringify(body.rsa.public)}`);
+                        envs.push(`"RSA_PUBLIC_KEY=${JSON.stringify(body.rsa.public)}"`);
                     // }
                     // if(!envs.includes('RSA_KEY')){
-                        envs.push(`"RSA_KEY=${JSON.stringify(body.rsa.private)}`);
+                        envs.push(`"RSA_KEY=${JSON.stringify(body.rsa.private)}"`);
                     // }
                     const result = await projectFactory.createProject(body, envs);
                     // delete result.parse.masterKey;
