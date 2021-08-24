813e848556c3775ad0710f0fd2cfdf5328bd234c
joshuamshana <mama27j@gmail.com>
2021-08-24 21:09:29 +0300

    update bfast database core lib

--- build-cloud.sh
+++ build-cloud.sh
@@ -1,5 +1,3 @@
-docker build --tag build-cloud .
-docker run --rm -v "${PWD}":/bfast build-cloud npm install --only=production
-npm config set //registry.npmjs.org/:_authToken="${NPM_TOKEN}"
+docker build --tag build-cloud . && docker run --rm -v "${PWD}":/bfast build-cloud npm install --only=production && npm config set //registry.npmjs.org/:_authToken="${NPM_TOKEN}"
 npm run test
 npm publish
