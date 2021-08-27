6938664df4ead7922e8b0975ec025accae98474d
joshuamshana <mama27j@gmail.com>
2021-08-27 10:10:14 +0300

    update database core

--- build-cloud.sh
+++
@@ -1,4 +0,0 @@
-docker build --tag build-cloud .
-rm -r node_modules
-docker run --rm -v "${PWD}":/bfast build-cloud npm install --production
-npm config set //registry.npmjs.org/:_authToken="${NPM_TOKEN}"
